e52bf49eef5598354f54e2972f4f5dc97c72b5b2
Erwan Jouan <erwanjouan@yahoo.fr>
2022-06-08 21:33:43 +0200

    update

---
+++ vars/deployToAwsEc2.groovy
@@ -0,0 +1,39 @@
+def call(String projectName, String projectVersion){
+    pipeline{
+        agent { label 'aws-cli' }
+        environment {
+            AWS_ACCESS_KEY_ID = credentials('aws_access_key_id')
+            AWS_SECRET_ACCESS_KEY = credentials('aws_secret_access_key')
+            AWS_DEFAULT_REGION='eu-west-1'
+        }
+        stages{
+            stage('Checkout Source') {
+                when { expression { return !params.DESTROY } }
+                steps {
+                    git branch: 'main', url: 'https://github.com/erwanjouan/' + projectName + '.git'
+                }
+            }
+            stage('Deploy Infrastructure') {
+                when { expression { return !params.DESTROY } }
+                steps {
+                    sh """
+                        aws cloudformation deploy \
+                            --capabilities CAPABILITY_NAMED_IAM \
+                            --template-file ./infra.yaml \
+                            --stack-name ${projectName} \
+                            --parameter-overrides \
+                                ProjectName=${projectName} \
+                                ProjectVersion=${projectVersion} \
+                                ImageTag=${DOCKER_TAG}
+                    """
+                }
+            }
+            stage('Destroy') {
+                when { expression { return params.DESTROY } }
+                steps {
+                    sh "aws cloudformation delete-stack --stack-name ${projectName}"
+                }
+            }
+        }
+    }
+}
