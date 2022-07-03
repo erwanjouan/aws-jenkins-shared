ae8a1fcd15705e0ff12ee75a42a0f06ec1d39160
Erwan Jouan <erwanjouan@yahoo.fr>
2022-07-03 11:02:07 +0200

    update

--- vars/destroyCfAwsStack.groovy
+++ vars/destroyCfAwsStack.groovy
@@ -18,8 +18,38 @@ def call(String infraProjectName){
             stage('Wait for destroy completion') {
                 steps {
                     sh """
-                        aws cloudformation wait stack-delete-complete \
-                            --stack-name ${infraProjectName}
+                        //aws cloudformation wait stack-delete-complete \
+                        //    --stack-name ${infraProjectName}
+
+                        local stack=${infraProjectName}
+                        local lastEvent
+                        local lastEventId
+                        local stackStatus=$(aws cloudformation describe-stacks --stack-name $stack | jq -c -r .Stacks[0].StackStatus)
+
+                        until \
+                            [ "$stackStatus" = "CREATE_COMPLETE" ] \
+                            || [ "$stackStatus" = "CREATE_FAILED" ] \
+                            || [ "$stackStatus" = "DELETE_COMPLETE" ] \
+                            || [ "$stackStatus" = "DELETE_FAILED" ] \
+                            || [ "$stackStatus" = "ROLLBACK_COMPLETE" ] \
+                            || [ "$stackStatus" = "ROLLBACK_FAILED" ] \
+                            || [ "$stackStatus" = "UPDATE_COMPLETE" ] \
+                            || [ "$stackStatus" = "UPDATE_ROLLBACK_COMPLETE" ] \
+                            || [ "$stackStatus" = "UPDATE_ROLLBACK_FAILED" ]; do
+                            
+                            #[[ $stackStatus == *""* ]] || [[ $stackStatus == *"CREATE_FAILED"* ]] || [[ $stackStatus == *"COMPLETE"* ]]; do
+                            lastEvent=$(aws cloudformation describe-stack-events --stack $stack --query 'StackEvents[].{ EventId: EventId, LogicalResourceId:LogicalResourceId, ResourceType:ResourceType, ResourceStatus:ResourceStatus, Timestamp: Timestamp }' --max-items 1 | jq .[0])
+                            eventId=$(echo "$lastEvent" | jq -r .EventId)
+                            if [ "$eventId" != "$lastEventId" ]
+                            then
+                                lastEventId=$eventId
+                                echo $(echo $lastEvent | jq -r '.Timestamp + "\t-\t" + .ResourceType + "\t-\t" + .LogicalResourceId + "\t-\t" + .ResourceStatus')
+                            fi
+                            sleep 3
+                            stackStatus=$(aws cloudformation describe-stacks --stack-name $stack | jq -c -r .Stacks[0].StackStatus)
+                        done
+
+                        echo "Stack Status: $stackStatus"
                     """
                 }
             }
